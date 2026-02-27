3ee96cbf8bcbc735d7a1c9bf7c8d17721e269a29
AriochButterfly <tomasstroud921@gmail.com>
2026-02-26 21:18:39 -0600

    Complete Operators and start on Lists

---
+++ Operators.py
@@ -0,0 +1,66 @@
+numbers = [1, 2, 3, 4, 5]
+fruits = ["apple", "banana", "cherry"]
+text = "Hello World"
+
+x = 5
+y = 4
+
+a = ["apple", "banana"]
+b = ["apple", "banana"]
+c = a
+
+sum1 = 100 + 50
+sum2 = sum1 + 250
+sum3 = sum2 + sum2
+print(sum1, sum2, sum3)
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+print(x / y)
+print(x // y)
+
+if (count := len(numbers)) > 3:
+    print(f"List has {count} elements.")
+
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+print(1 < x < 10)
+print(1 < x and x < 10)
+print(x > 0 and x < 10)
+print(x < 5 or x > 10)
+print(not(x > 3 and x < 10))
+
+print(a is c)
+print(a is b)
+print(a == b)
+print(a is not b)
+
+print(a == b)
+print(a is b)
+
+print("banana" in fruits)
+print("pineapple" not in fruits)
+
+print("H" in text)
+print("hello" in text)
+print("z" not in text)
+
+print(6 & 3)
+print(6 | 3)
+print(6 ^ 3)
+
+print((6 + 3) - (6 + 3))
+print(100 + 5 * 3)
+print(5 + 4 - 7 + 3)
